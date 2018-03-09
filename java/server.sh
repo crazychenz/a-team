baf9aa4f8279d5634f7565782251512b6bf7bac7
Crazy Chenz <crazychenz@gmail.com>
2018-03-09 11:08:00 -0500

    Added posix shell scripts for client and server for easier testing of command line arguments.

---
+++ java/server.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# TODO: Fix this
+#  -Djava.util.logging.manager=org.apache.logging.log4j.jul.LogManager \
+
+java -cp lib/*:dist/clueless.jar \
+  -Dlog4j.configurationFile=log4j2.xml \
+  -DlogFilename=client.log \
+  ServerRun $@
