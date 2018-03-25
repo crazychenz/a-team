e3334501b7a23018cd795321da74af7275b15f5d
Crazy Chenz <crazychenz@gmail.com>
2018-03-24 20:29:23 -0400

    Refactored BoardBuilder to work without CardsEnum

--- java/src/clueless/client/cli/BoardBuilder.java
+++ java/src/clueless/client/cli/BoardBuilder.java
@@ -3,6 +3,11 @@ package clueless.client.cli;
 
 import clueless.*;
 import clueless.client.*;
+import de.vandermeer.asciitable.AsciiTable;
+import de.vandermeer.asciithemes.TA_GridThemes;
+import de.vandermeer.skb.interfaces.transformers.textformat.TextAlignment;
+import java.util.HashMap;
+import java.util.Map.Entry;
 
 /** @author tombo */
 public class BoardBuilder {
@@ -14,89 +19,92 @@ public class BoardBuilder {
     }
 
     public String generateBoard() {
-        String rend = "";
-        /*
-              AsciiTable at = new AsciiTable();
-              at.addRule();
-              at.addRow(
-                      "Study* - " + returnSuspectsInRoom(CardsEnum.LOCATION_STUDY),
-                      returnSuspectsInRoom(CardsEnum.HALLWAY_STUDY_HALL),
-                      "Hall - " + returnSuspectsInRoom(CardsEnum.LOCATION_HALL),
-                      returnSuspectsInRoom(CardsEnum.HALLWAY_HALL_LOUNGE),
-                      "*Lounge - " + returnSuspectsInRoom(CardsEnum.LOCATION_LOUNGE));
-              at.addRow(
-                      " " + returnSuspectsInRoom(CardsEnum.HALLWAY_STUDY_LIBRARY),
-                      "",
-                      " " + returnSuspectsInRoom(CardsEnum.HALLWAY_HALL_BILLIARD),
-                      "",
-                      " " + returnSuspectsInRoom(CardsEnum.HALLWAY_LOUNGE_DINING));
-              at.addRow(
-                      "Library - " + returnSuspectsInRoom(CardsEnum.LOCATION_LIBRARY),
-                      returnSuspectsInRoom(CardsEnum.HALLWAY_LIBRARY_BILLIARD),
-                      "Billiard Room - " + returnSuspectsInRoom(CardsEnum.LOCATION_BILLIARDROOM),
-                      returnSuspectsInRoom(CardsEnum.HALLWAY_DINING_BILLIARD),
-                      "Dining Room - " + returnSuspectsInRoom(CardsEnum.LOCATION_DININGROOM));
-              at.addRow(
-                      " " + returnSuspectsInRoom(CardsEnum.HALLWAY_CONSERVATORY_LIBRARY),
-                      "",
-                      " " + returnSuspectsInRoom(CardsEnum.HALLWAY_BALL_BILLIARD),
-                      "",
-                      " " + returnSuspectsInRoom(CardsEnum.HALLWAY_DINING_KITCHEN));
-              at.addRow(
-                      "Conservatory* - " + returnSuspectsInRoom(CardsEnum.LOCATION_CONSERVATORY),
-                      returnSuspectsInRoom(CardsEnum.HALLWAY_BALL_CONSERVATORY),
-                      "Ballroom - " + returnSuspectsInRoom(CardsEnum.LOCATION_BALLROOM),
-                      returnSuspectsInRoom(CardsEnum.HALLWAY_KITCHEN_BALL),
-                      "*Kitchen - " + returnSuspectsInRoom(CardsEnum.LOCATION_KITCHEN));
-              at.addRule();
-              at.getContext().setWidth(140);
-              at.getContext().setGridTheme(TA_GridThemes.OUTSIDE);
-              at.setTextAlignment(TextAlignment.CENTER);
-              String rend = at.render();
-        */
+
+        AsciiTable at = new AsciiTable();
+        at.addRule();
+        at.addRow(
+                "Study* - " + returnSuspectsInRoom(Room.LOCATION_STUDY),
+                returnSuspectsInRoom(Hallway.HALLWAY_STUDY_HALL),
+                "Hall - " + returnSuspectsInRoom(Room.LOCATION_HALL),
+                returnSuspectsInRoom(Hallway.HALLWAY_HALL_LOUNGE),
+                "*Lounge - " + returnSuspectsInRoom(Room.LOCATION_LOUNGE));
+        at.addRow(
+                " " + returnSuspectsInRoom(Hallway.HALLWAY_STUDY_LIBRARY),
+                "",
+                " " + returnSuspectsInRoom(Hallway.HALLWAY_HALL_BILLIARD),
+                "",
+                " " + returnSuspectsInRoom(Hallway.HALLWAY_LOUNGE_DINING));
+        at.addRow(
+                "Library - " + returnSuspectsInRoom(Room.LOCATION_LIBRARY),
+                returnSuspectsInRoom(Hallway.HALLWAY_LIBRARY_BILLIARD),
+                "Billiard Room - " + returnSuspectsInRoom(Room.LOCATION_BILLIARDROOM),
+                returnSuspectsInRoom(Hallway.HALLWAY_DINING_BILLIARD),
+                "Dining Room - " + returnSuspectsInRoom(Room.LOCATION_DININGROOM));
+        at.addRow(
+                " " + returnSuspectsInRoom(Hallway.HALLWAY_CONSERVATORY_LIBRARY),
+                "",
+                " " + returnSuspectsInRoom(Hallway.HALLWAY_BALL_BILLIARD),
+                "",
+                " " + returnSuspectsInRoom(Hallway.HALLWAY_DINING_KITCHEN));
+        at.addRow(
+                "Conservatory* - " + returnSuspectsInRoom(Room.LOCATION_CONSERVATORY),
+                returnSuspectsInRoom(Hallway.HALLWAY_BALL_CONSERVATORY),
+                "Ballroom - " + returnSuspectsInRoom(Room.LOCATION_BALLROOM),
+                returnSuspectsInRoom(Hallway.HALLWAY_KITCHEN_BALL),
+                "*Kitchen - " + returnSuspectsInRoom(Room.LOCATION_KITCHEN));
+        at.addRule();
+        at.getContext().setWidth(140);
+        at.getContext().setGridTheme(TA_GridThemes.OUTSIDE);
+        at.setTextAlignment(TextAlignment.CENTER);
+        String rend = at.render();
+
         return rend;
     }
 
-    private String returnSuspectsInRoom(RoomCard room) {
+    private String returnSuspectsInRoom(Location room) {
         // Not sure why this was changed lol it worked before. had to fix it
-        String toReturn = "["; /*
-        // if (clientState.getGameState().getSuspectLocations().containsValue(room)) {
-        // for (Entry<CardsEnum, CardsEnum> entry :
-        //        clientState.getGameState().getSuspectLocations().entrySet()) {
-        for (Suspect entry : clientState.getGameState().getSuspectLocations()) {
-            CardsEnum location = entry.getCurrent_location();
-            if (location == room) {
-                toReturn += entry.getSuspect().getLabel() + ",";
-                // toReturn += entry.getKey().getLabel() + ",";
-            }
-        }
+        String toReturn = "[";
+        HashMap<SuspectCard, Integer> map;
 
-        // Remove last comma, only if there are suspects in the room
-        if (toReturn.length() > 1) {
-            toReturn = toReturn.substring(0, toReturn.length() - 1);
+        map = clientState.getGameState().getSuspectLocations();
+        if (map.containsValue(room.getId())) {
+            for (Entry<SuspectCard, Integer> entry : map.entrySet()) {
+                if (entry.getValue() == room.getId()) {
+                    toReturn += entry.getKey().getName() + ",";
+                }
+            }
+
+            // Remove last comma, only if there are suspects in the room
+            if (toReturn.length() > 1) {
+                toReturn = toReturn.substring(0, toReturn.length() - 1);
+            }
         }
 
         toReturn += "]";
         toReturn += "\n";
-        toReturn += returnWeaponsInRoom(room);*/
+        toReturn += returnWeaponsInRoom(room);
         return toReturn;
     }
 
-    private String returnWeaponsInRoom(RoomCard room) {
+    private String returnWeaponsInRoom(Location room) {
         String toReturn = "{";
-        /*for (Weapon entry : clientState.getGameState().getWeaponLocations()) {
-            CardsEnum location = entry.getCurrent_location();
-            if (location == room) {
-                toReturn += entry.getWeapon().getLabel() + ",";
+
+        HashMap<WeaponCard, Integer> map;
+        map = clientState.getGameState().getWeaponLocations();
+        if (map.containsValue(room.getId())) {
+            for (Entry<WeaponCard, Integer> entry : map.entrySet()) {
+                if (entry.getValue() == room.getId()) {
+                    toReturn += entry.getKey().getName() + ",";
+                }
+            }
+
+            // Remove last comma, only if there are suspects in the room
+            if (toReturn.length() > 1) {
+                toReturn = toReturn.substring(0, toReturn.length() - 1);
             }
         }
 
-        // Remove last comma, only if there are weapons in the room
-        if (toReturn.length() > 1) {
-            toReturn = toReturn.substring(0, toReturn.length() - 1);
-        }
-
-        toReturn += "}";*/
+        toReturn += "}";
         return toReturn;
     }
 }
