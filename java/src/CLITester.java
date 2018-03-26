d9aa24e5312e97c0262a26c50778bce71f6206e0
Crazy Chenz <crazychenz@gmail.com>
2018-03-26 08:31:17 -0400

    Updated CLITester.java to play a complete game

--- java/src/CLITester.java
+++ java/src/CLITester.java
@@ -1,4 +1,6 @@
 import clueless.client.cli.*;
+import clueless.server.Server;
+import java.io.PrintStream;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -14,30 +16,110 @@ public class CLITester {
         logger.info("PASS: " + desc);
     }
 
+    public static void sleep(long millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (Exception e) {
+        }
+    }
+
     public static void main(String[] args) {
+        PrintStream out = System.out;
         String resp;
         logger.info("Starting CLI tester.");
+
+        logger.info("Initializing the server thread.");
+        try {
+            Server server = new Server(1);
+            Thread serverThread = new Thread(server);
+            serverThread.start();
+            Thread.sleep(1000);
+        } catch (Exception e) {
+            logger.error("Something went wrong in the server.");
+            e.printStackTrace();
+            System.exit(-1);
+        }
+
+        logger.info("Initializing the CLI environment.");
         CLI.init(args);
 
         // Create all possible clients.
         CLI cli1 = new CLI();
-        CLI cli2 = new CLI();
-        CLI cli3 = new CLI();
-        CLI cli4 = new CLI();
-        CLI cli5 = new CLI();
-        CLI cli6 = new CLI();
+        CLI peacock = new CLI();
+        CLI scarlet = new CLI();
+        CLI white = new CLI();
+        CLI green = new CLI();
+        CLI mustard = new CLI();
 
+        // Register Everyone
         resp = CLI.handleCommand(cli1, "config Plum\n");
         assert_null(resp, "Configure Player #1");
-        resp = CLI.handleCommand(cli2, "config Peacock\n");
+        resp = CLI.handleCommand(peacock, "config Peacock\n");
         assert_null(resp, "Configure Player #2");
-        resp = CLI.handleCommand(cli3, "config Scarlet\n");
+        resp = CLI.handleCommand(scarlet, "config Scarlet\n");
         assert_null(resp, "Configure Player #3");
-        resp = CLI.handleCommand(cli4, "config White\n");
+        resp = CLI.handleCommand(white, "config White\n");
         assert_null(resp, "Configure Player #4");
-        resp = CLI.handleCommand(cli5, "config Green\n");
+        resp = CLI.handleCommand(green, "config Green\n");
         assert_null(resp, "Configure Player #5");
-        resp = CLI.handleCommand(cli6, "config Mustard\n");
+        resp = CLI.handleCommand(mustard, "config Mustard\n");
         assert_null(resp, "Configure Player #6");
+
+        // Start the game
+        resp = CLI.handleCommand(scarlet, "start\n");
+        sleep(5000);
+
+        // Scarlet should always go first
+        resp = CLI.handleCommand(scarlet, "move east\n");
+        out.println(resp);
+        sleep(1000);
+
+        // Check where we are
+        resp = CLI.handleCommand(scarlet, "board\n");
+        out.println(resp);
+        sleep(1000);
+
+        // Make suggestion
+        resp = CLI.handleCommand(scarlet, "suggest Mustard Rope\n");
+        out.println(resp);
+        sleep(1000);
+
+        resp = CLI.handleCommand(mustard, "disprove\n");
+        out.println(resp);
+        sleep(1000);
+
+        resp = CLI.handleCommand(white, "disprove\n");
+        out.println(resp);
+        sleep(1000);
+
+        resp = CLI.handleCommand(green, "disprove\n");
+        out.println(resp);
+        sleep(1000);
+
+        resp = CLI.handleCommand(peacock, "disprove Mustard\n");
+        out.println(resp);
+        sleep(1000);
+
+        resp = CLI.handleCommand(scarlet, "done\n");
+        out.println(resp);
+        sleep(1000);
+
+        // BUG: It doesn't like the quote
+        // resp = CLI.handleCommand(mustard, "chat I told you I didn\'t do it!\n");
+        // out.println(resp);
+        // sleep(1000);
+
+        // BUG: Items with spaces aren't working correctly. Example: 'Lead Pipe'
+        resp = CLI.handleCommand(mustard, "accuse Peacock Study Pipe\n");
+        out.println(resp);
+        sleep(1000);
+
+        // Kill all the clients
+        resp = CLI.handleCommand(cli1, "exit\n");
+        resp = CLI.handleCommand(peacock, "exit\n");
+        resp = CLI.handleCommand(scarlet, "exit\n");
+        resp = CLI.handleCommand(white, "exit\n");
+        resp = CLI.handleCommand(green, "exit\n");
+        resp = CLI.handleCommand(mustard, "exit\n");
     }
 }
